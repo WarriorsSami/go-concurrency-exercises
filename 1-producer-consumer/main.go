cd589483f5f624accdc5794fe7c65b7d4379e5b8
WarriorsSami <nicusor.barbut.v4j@student.ucv.ro>
2024-05-06 12:56:00 +0300

    feat(producer-consumer): complete challenge #1 using a goroutines, channels, wg and for-select

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -9,27 +9,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweetChan chan *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
 		tweet, err := stream.Next()
-		if err == ErrEOF {
-			return tweets
+		if errors.Is(err, ErrEOF) {
+			close(tweetChan)
+			return
 		}
 
-		tweets = append(tweets, tweet)
+		tweetChan <- tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
-		if t.IsTalkingAboutGo() {
-			fmt.Println(t.Username, "\ttweets about golang")
-		} else {
-			fmt.Println(t.Username, "\tdoes not tweet about golang")
+func consumer(tweetChan chan *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case t, ok := <-tweetChan:
+			// check if channel is closed
+			if !ok {
+				return
+			}
+
+			if t.IsTalkingAboutGo() {
+				fmt.Println(t.Username, "\ttweets about golang")
+			} else {
+				fmt.Println(t.Username, "\tdoes not tweet about golang")
+			}
 		}
 	}
 }
@@ -38,11 +51,18 @@ func main() {
 	start := time.Now()
 	stream := GetMockStream()
 
+	tweetChan := make(chan *Tweet)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	// Producer
-	tweets := producer(stream)
+	go producer(stream, tweetChan, &wg)
 
 	// Consumer
-	consumer(tweets)
+	go consumer(tweetChan, &wg)
+
+	wg.Wait()
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
