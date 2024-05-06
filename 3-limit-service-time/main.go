e921f9d06ef56cab96fa029a4b848f833ec0d3b7
WarriorsSami <nicusor.barbut.v4j@student.ucv.ro>
2024-05-06 17:56:20 +0300

    feat(limit-service-time): complete challenge #3 beginner-level using a timeout context and a signal channel

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,11 @@
 
 package main
 
+import (
+	"context"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +26,21 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	done := make(chan bool, 1)
+	go func() {
+		process()
+		done <- true
+	}()
+
+	select {
+	case <-done:
+		return true
+	case <-ctx.Done():
+		return u.IsPremium
+	}
 }
 
 func main() {
