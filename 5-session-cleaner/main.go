11743b827531e1a0a9b42b7d2e084f8efc1232d5
WarriorsSami <nicusor.barbut.v4j@student.ucv.ro>
2024-05-06 21:26:42 +0300

    feat(session-cleaner): complete challenge #5 using rwmutex and tickers

--- 5-session-cleaner/main.go
+++ 5-session-cleaner/main.go
@@ -20,25 +20,70 @@ package main
 import (
 	"errors"
 	"log"
+	"sync"
+	"time"
 )
+
+// ICleaner is an interface for cleaning idle sessions
+type ICleaner interface {
+	Clean(sessionManager *SessionManager)
+}
+
+// SessionCleaner is a cleaner that cleans idle sessions
+type SessionCleaner struct {
+	clock *time.Ticker
+	done  chan interface{}
+}
+
+// NewSessionCleaner creates a new sessionCleaner
+func NewSessionCleaner() *SessionCleaner {
+	return &SessionCleaner{
+		clock: time.NewTicker(1 * time.Second),
+	}
+}
+
+// Clean cleans idle sessions
+func (c *SessionCleaner) Clean(sm *SessionManager) {
+	for {
+		select {
+		case <-c.clock.C:
+			sm.rwLock.Lock()
+
+			for sessionID, session := range sm.sessions {
+				if time.Since(session.updatedAt) > 5*time.Second {
+					delete(sm.sessions, sessionID)
+				}
+			}
+
+			sm.rwLock.Unlock()
+		case <-c.done:
+			return
+		}
+	}
+}
 
 // SessionManager keeps track of all sessions from creation, updating
 // to destroying.
 type SessionManager struct {
-	sessions map[string]Session
+	sessions       map[string]Session
+	rwLock         sync.RWMutex
+	sessionCleaner ICleaner
 }
 
 // Session stores the session's data
 type Session struct {
-	Data map[string]interface{}
+	Data      map[string]interface{}
+	updatedAt time.Time
 }
 
 // NewSessionManager creates a new sessionManager
 func NewSessionManager() *SessionManager {
 	m := &SessionManager{
-		sessions: make(map[string]Session),
+		sessions:       make(map[string]Session),
+		sessionCleaner: NewSessionCleaner(),
 	}
 
+	go m.sessionCleaner.Clean(m)
 	return m
 }
 
@@ -49,8 +94,12 @@ func (m *SessionManager) CreateSession() (string, error) {
 		return "", err
 	}
 
+	m.rwLock.Lock()
+	defer m.rwLock.Unlock()
+
 	m.sessions[sessionID] = Session{
-		Data: make(map[string]interface{}),
+		Data:      make(map[string]interface{}),
+		updatedAt: time.Now(),
 	}
 
 	return sessionID, nil
@@ -63,6 +112,9 @@ var ErrSessionNotFound = errors.New("SessionID does not exists")
 // GetSessionData returns data related to session if sessionID is
 // found, errors otherwise
 func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{}, error) {
+	m.rwLock.RLock()
+	defer m.rwLock.RUnlock()
+
 	session, ok := m.sessions[sessionID]
 	if !ok {
 		return nil, ErrSessionNotFound
@@ -72,14 +124,22 @@ func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{
 
 // UpdateSessionData overwrites the old session data with the new one
 func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]interface{}) error {
+	m.rwLock.RLock()
+
 	_, ok := m.sessions[sessionID]
 	if !ok {
+		defer m.rwLock.RUnlock()
 		return ErrSessionNotFound
 	}
+	m.rwLock.RUnlock()
+
+	m.rwLock.Lock()
+	defer m.rwLock.Unlock()
 
 	// Hint: you should renew expiry of the session here
 	m.sessions[sessionID] = Session{
-		Data: data,
+		Data:      data,
+		updatedAt: time.Now(),
 	}
 
 	return nil
